d992dfc3cfb603e58151bc2f1a1b7972a9bb75be
Alisson Lima <amlxd5@gmail.com>
2022-07-28 23:19:32 -0500

    Criação do projeto Spring/Maven

---
+++ crud-firebase/src/main/java/br/alissonlimabr/crudfirebase/CrudFirebaseApplication.java
@@ -0,0 +1,13 @@
+package br.alissonlimabr.crudfirebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudFirebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudFirebaseApplication.class, args);
+	}
+
+}
---
+++ crud-firebase/src/test/java/br/alissonlimabr/crudfirebase/CrudFirebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.alissonlimabr.crudfirebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFirebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
